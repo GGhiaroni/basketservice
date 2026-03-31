a48b9a82ea3db43c8fbd08227cde3e437b48f3e8
GGhiaroni <gghiaronitiziano@gmail.com>
2026-03-31 11:53:28 -0300

    feat: Cria a interface com os dois métodos getAllProducts e getProductById, implementando o FeignClient para conexão com a API

---
+++ src/main/java/GabrielTiziano/ecommerce/basketservice/client/PlatziStoreClient.java
@@ -0,0 +1,18 @@
+package GabrielTiziano.ecommerce.basketservice.client;
+
+import GabrielTiziano.ecommerce.basketservice.response.PlatziProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "PlatziStoreClient", url = "${basket.client.platzi}")
+public interface PlatziStoreClient {
+
+    @GetMapping("/products")
+    List<PlatziProductResponse> getAllProducts();
+
+    @GetMapping("/products/{id}")
+    PlatziProductResponse getProductById(@PathVariable Long id);
+}
