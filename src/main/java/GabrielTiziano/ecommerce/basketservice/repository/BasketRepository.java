132bd2d577c5021fae5dd4540eb19c69f560af3f
GGhiaroni <gghiaronitiziano@gmail.com>
2026-04-03 11:14:26 -0300

    feat: Implementa o Repository de Basket

---
+++ src/main/java/GabrielTiziano/ecommerce/basketservice/repository/BasketRepository.java
@@ -0,0 +1,14 @@
+package GabrielTiziano.ecommerce.basketservice.repository;
+
+import GabrielTiziano.ecommerce.basketservice.entity.Basket;
+import GabrielTiziano.ecommerce.basketservice.entity.Status;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface BasketRepository extends MongoRepository<Basket, String> {
+
+    Optional<Basket> findByClientAndStatus(Long client, Status status);
+
+}
+
